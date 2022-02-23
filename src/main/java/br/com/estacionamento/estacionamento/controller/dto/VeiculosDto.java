eee5ee82e8118e4c28b5a9197c657a261669b032
EvelynGalvao <evilenluhana@gmail.com>
2022-02-23 15:53:30 -0300

    commit on master

--- src/main/java/br/com/estacionamento/estacionamento/controller/dto/VeiculosDto.java
+++ src/main/java/br/com/estacionamento/estacionamento/controller/dto/VeiculosDto.java
@@ -1,4 +1,38 @@
 package br.com.estacionamento.estacionamento.controller.dto;
 
+import br.com.estacionamento.estacionamento.model.Veiculo;
+import org.springframework.boot.autoconfigure.amqp.ConnectionFactoryCustomizer;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class VeiculosDto {
+
+    private Connection connection;
+
+    String Id;
+    String Marca;
+    String Modelo;
+    String Cor;
+    String Placa;
+    String Tipo;
+
+    public VeiculosDto() {
+
+        this.connection = new ConnectionFactory().getConnection();
+    }
+
+    public void adiciona(Veiculo veiculo) {
+
+
+    }
+
+    private class ConnectionFactory {
+        public Connection getConnection() {
+
+            return null;
+        }
+    }
 }
--- src/main/java/br/com/estacionamento/estacionamento/model/Veiculo.java
+++ src/main/java/br/com/estacionamento/estacionamento/model/Veiculo.java
@@ -15,16 +15,54 @@ public class Veiculo {
     private String Placa;
     private String Tipo;
 
-    public Veiculo( String marca, String modelo, String cor, String placa, String tipo) {
+   // public Veiculo( String marca, String modelo, String cor, String placa, String tipo) {
 
-        Marca = marca;
-        Modelo = modelo;
-        Cor = cor;
-        Placa = placa;
-        Tipo = tipo;
+        //Marca = marca;
+        //Modelo = modelo;
+        //Cor = cor;
+        //Placa = placa;
+        //Tipo = tipo;
 
-
-    }
     public Veiculo(){
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public String getCor() {
+        return Cor;
+    }
+
+    public void setCor(String cor) {
+        Cor = cor;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public void setPlaca(String placa) {
+        Placa = placa;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
     }
 }
